d04de28cebd7bdc3e229646b135fe4d30b7259ea
盛海滨 <15972660547@163.com>
2022-11-09 20:12:55 +0800

    user

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,15 +1,24 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './entities/user.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository, Like } from 'typeorm';
 
 @Injectable()
 export class UserService {
+  constructor(
+    @InjectRepository(User) private readonly user: Repository<User>,
+  ) {}
   create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+    const data = new User();
+    data.username = 'guagua';
+    data.password = '123456';
+    this.user.save(data);
   }
 
   findAll() {
-    return `This action returns all user`;
+    return 'dududud';
   }
 
   findOne(id: number) {
